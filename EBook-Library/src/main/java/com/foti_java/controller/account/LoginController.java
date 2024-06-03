c0a347622b6d9ca47435cb8c8cc04ed777421c25
kimtuyen2511 <nguyentuyentctv2021@gmail.com>
2024-06-03 22:28:05 +0700

    linkHeader

--- EBook-Library/src/main/java/com/foti_java/controller/account/LoginController.java
+++ EBook-Library/src/main/java/com/foti_java/controller/account/LoginController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("Ebook")
+@RequestMapping("Ebook/account")
 public class LoginController {
 	@RequestMapping("login")
 	public String getChangePass() {
