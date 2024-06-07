a15ac12d444f5312e5f892629409039789423912
kimtuyen2511 <nguyentuyentctv2021@gmail.com>
2024-06-07 21:15:45 +0700

    push

--- EBook-Library/src/main/java/com/foti_java/repository/VoucherRepository.java
+++ EBook-Library/src/main/java/com/foti_java/repository/VoucherRepository.java
@@ -1,9 +1,14 @@
 package com.foti_java.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.foti_java.model.TypeVoucher;
 import com.foti_java.model.Voucher;
 
 public interface VoucherRepository extends JpaRepository<Voucher, Integer> {
-
+	Page<Voucher> findAllByStatus(boolean status,Pageable pageable);
+	Page<Voucher> findAllByTypeVoucher(TypeVoucher typeVoucher,Pageable pageable);
+	Page<Voucher> findAllByStatusAndTypeVoucher(boolean status, TypeVoucher typeVoucher,Pageable pageable);
 }
