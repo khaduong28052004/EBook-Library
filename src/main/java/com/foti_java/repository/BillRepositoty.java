0306805c628894457b0570866c9883e043439196
khadv28 <khaduong28052004@gmail.com>
2024-06-10 14:58:32 +0700

    xóa thư mục rác

--- src/main/java/com/foti_java/repository/BillRepositoty.java
+++
@@ -1,9 +0,0 @@
-package com.foti_java.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.foti_java.model.Bill;
-
-public interface BillRepositoty extends JpaRepository<Bill, Integer> {
-
-}
