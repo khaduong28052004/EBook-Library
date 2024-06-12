fa519c23ed47a401532193b5f865feee5a9295af
phuckemz03 <146057737+phuckemz03@users.noreply.github.com>
2024-06-13 04:05:53 +0700

    product-bank-billSeller-homeSeller

--- EBook-Library/src/main/java/com/foti_java/repository/ProductRepository.java
+++ EBook-Library/src/main/java/com/foti_java/repository/ProductRepository.java
@@ -47,4 +47,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	Page<Product> findAllByNameContaining(String name, Pageable pageable);
 //	@Query("SELECT p FROM Product p  WHERE p.cartDetail = listCart")
 //	List<Product> listCart(@Param("listCart")CartDetail listCart);
+
+	List<Product> findAllByActiveAndStatus(boolean active, boolean status);
+    @Query("SELECT p FROM Product p WHERE p.status = true and p.account.id =:id")
+    public List<Product> findByStatus(@Param("id") Integer id);
 }
--- EBook-Library/src/main/java/com/foti_java/repository/RoleDetailRepository.java
+++ EBook-Library/src/main/java/com/foti_java/repository/RoleDetailRepository.java
@@ -1,9 +1,12 @@
 package com.foti_java.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.foti_java.model.Account;
 import com.foti_java.model.RoleDetail;
 
 public interface RoleDetailRepository extends JpaRepository<RoleDetail, Integer>{
-
+	List<RoleDetail> findByAccount(Account account);
 }
